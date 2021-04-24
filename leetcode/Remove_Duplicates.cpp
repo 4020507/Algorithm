7ff58b40a905f92854f15cb133a244bfa54a67c3
4020507 <55306169+4020507@users.noreply.github.com>
2021-04-24 11:57:35 +0900

    Create Remove_Duplicates.cpp

---
+++ leetcode/Remove_Duplicates.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int size = nums.size();
+        
+        for(int i = 1;i<size;i++)
+        {
+            if(nums[i] == nums[i-1])
+            {
+                nums.erase(nums.begin()+i);
+                i--;
+                size = nums.size();
+            }
+        }
+        return size;
+    }
+};
