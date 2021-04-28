90d4ffeb91b02d4aa523dbc8fdbbb9d4438a92f7
4020507 <55306169+4020507@users.noreply.github.com>
2021-04-28 19:54:38 +0900

    Create mergeTwoLists.cpp

---
+++ leetcode/mergeTwoLists.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(!l1 && !l2)
+            return NULL;
+       ListNode * ans1 = new ListNode();
+        ListNode * ans = ans1;
+        
+        
+        while(l1 && l2)
+        {
+            if(l1->val > l2->val)
+            {
+                ans->next = l2;
+                l2 = l2->next;
+            }
+            else
+            {
+                ans->next = l1;
+                l1 = l1->next;
+            }
+            ans = ans->next;
+        }
+        if(l1)
+        {
+            ans->next = l1;
+        }
+        if(l2)
+        {
+            ans->next = l2;
+        }
+        
+        return ans1->next;
+        
+    }
+};
