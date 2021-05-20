1c810803605a949bca12e59b1d38a222f4ffd3ce
4020507 <55306169+4020507@users.noreply.github.com>
2021-05-20 15:07:58 +0900

    Create moveZeroes.cpp

---
+++ leetcode/moveZeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        int size = nums.size();
+        
+        for(int i = 0;i<size;i++)
+        {
+            if(nums[i] != 0)
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        for(int i = j;i<size;i++)
+            nums[i] = 0;
+    }
+};
