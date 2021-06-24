68cef4baf4a0bc0f29fff33dd9edaadf4c886946
4020507 <55306169+4020507@users.noreply.github.com>
2021-06-24 09:55:50 +0900

    Create prefix.cpp

---
+++ Programmers/level2/prefix.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    
+    sort(phone_book.begin(),phone_book.end());
+    int len = phone_book.size();
+    for(int i = 0;i<len-1;i++)
+    {
+        int size = phone_book[i].size();
+        if(phone_book[i] == phone_book[i+1].substr(0,size))
+        {
+            return false;
+        }
+    }
+    return answer;
+}
