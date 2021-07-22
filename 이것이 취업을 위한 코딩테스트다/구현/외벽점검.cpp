676ef59b5d947ccaa6606b1a57278e16d4d52456
4020507 <55306169+4020507@users.noreply.github.com>
2021-07-22 17:03:10 +0900

    Create 외벽점검.cpp

---
+++ 이것이 취업을 위한 코딩테스트다/구현/외벽점검.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(int n, vector<int> weak, vector<int> dist) {
+    int answer = dist.size() + 1;
+    
+    int len = weak.size();
+    
+    for(int i = 0;i<len;i++)
+    {
+        weak.push_back(weak[i] + n);
+    }
+    
+    for(int i = 0;i<len;i++)
+    {
+        do
+        {
+            int count = 1;
+            int pos = weak[i] + dist[count - 1];
+            
+            for(int j = i;j<i+len;j++)
+            {
+                if(pos < weak[j])
+                {
+                    count++;
+                    if(count >dist.size())
+                        break;
+                    pos += weak[j] + dist[count - 1];
+                }
+            }
+            answer = min(answer,count);
+        }while(next_permutation(dist.begin(),dist.end()));
+    }
+    return answer;
+}
