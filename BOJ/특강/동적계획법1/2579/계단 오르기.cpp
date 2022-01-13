a58759cadadfd7d71ca6742f92125316eb8cbd67
Maxito <55306169+4020507@users.noreply.github.com>
2022-01-13 10:50:22 +0900

    Create 계단 오르기.cpp

---
+++ BOJ/특강/동적계획법1/2579/계단 오르기.cpp
@@ -0,0 +1,36 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+#define MAX 302
+
+int dp[MAX];
+int arr[MAX];
+int main(void)
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int N;
+    cin >> N;
+
+    for (int i = 1;i <= N;i++)
+        cin >> arr[i];
+    dp[1] = arr[1];
+    dp[2] = arr[1] + arr[2];
+    dp[3] = max(arr[1] + arr[3], arr[2] + arr[3]);
+    for (int i = 3;i <= N;i++)
+    {
+        dp[i] = max(dp[i-2] + arr[i],dp[i-3] + arr[i-1]+arr[i]);
+    }
+
+    cout << dp[N];
+
+    return 0;
+
+}
